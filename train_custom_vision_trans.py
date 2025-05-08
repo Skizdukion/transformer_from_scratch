65218313f7ffc7d0d9ce31f7c6dd499d654d870c
Skizdukion <rider993sinus@gmail.com>
2025-05-08 16:34:06 +0700

    make dataset into one folder

--- train_custom_vision_trans.py
+++ train_custom_vision_trans.py
@@ -5,9 +5,9 @@ from block.encoder import EncoderBlock, FlexScaleEncoderBlock, Encoder
 from block.feed_forward import FeedForwardBlock
 from block.multihead_attention import MultiHeadAttention
 from block.vision_transformer import CustomClassifyVisionTransformer
+from dataset.vision_dataset import get_cifar10_vision_ds
 from utils.weight_retrieve import get_weights_file_path, latest_weights_file_path
 from custom_vision_trans.config import get_config
-from custom_vision_trans.dataset import get_ds
 from torch.utils.tensorboard import SummaryWriter
 import torch.nn as nn
 import warnings
@@ -58,7 +58,7 @@ def train_model():
 
     Path(config["model_folder"]).mkdir(parents=True, exist_ok=True)
 
-    (train_data_loader, val_data_loader, num_classes) = get_ds()
+    (train_data_loader, val_data_loader, num_classes) = get_cifar10_vision_ds()
 
     flexscale_blocks = []
 
